3092d30370cb5dbec76f814b17e454948db4e948
jaydip <vananijaydip53@gmail.com>
2024-06-04 11:04:34 +0530

    11:40

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import TodoList from "./componant/TodoList";
+import { useDispatch } from "react-redux";
+import { fetchdata } from "./Toolkit/Slice/todoSlice";
+
 
 function App() {
+
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+
+    dispatch(fetchdata({ endpoint: "gettask" }))
+    dispatch(fetchdata({ endpoint: "gettaskstatuscomplete" }));
+    dispatch(fetchdata({ endpoint: "gettaskstatusuncomplete" }));
+
+
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
